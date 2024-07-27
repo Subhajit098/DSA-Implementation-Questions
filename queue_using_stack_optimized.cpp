5f9cf0f32f03658fdf2e6220481550ebe818cf83
androidjit07 <sd.20u10020@btech.nitdgp.ac.in>
2024-07-27 22:17:15 +0530

    queue using stack optimized

---
+++ queue_using_stack_optimized.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class MyQueue
+{
+public:
+    stack<int> s1, s2;
+    MyQueue()
+    {
+    }
+
+    void push(int x)
+    {
+        s1.push(x);
+    }
+
+    int pop()
+    {
+        int x = -1;
+        if (s2.size() != 0)
+        {
+            x = s2.top();
+            s2.pop();
+            return x;
+        }
+        else
+        {
+            while (s1.size())
+            {
+                s2.push(s1.top());
+                s1.pop();
+            }
+            if (s2.size() != 0)
+            {
+                x = s2.top();
+                s2.pop();
+            }
+        }
+        return x;
+    }
+
+    int peek()
+    {
+        if (s2.size() != 0)
+        {
+            return s2.top();
+        }
+        int x = -1;
+        while (s1.size())
+        {
+            s2.push(s1.top());
+            s1.pop();
+        }
+        if (s2.size() != 0)
+        {
+            x = s2.top();
+        }
+        return x;
+    }
+
+    bool empty()
+    {
+        if (s2.size() != 0)
+            return false;
+        while (s1.size() != 0)
+        {
+            s2.push(s1.top());
+            s1.pop();
+        }
+        return s2.empty();
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
